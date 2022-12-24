0cad81d9a7f49f73e19dbfea107f1c300eff8ccb
Su <dev@jianchang.su>
2022-12-24 14:25:14 +0100

    Update init_installation.sh

--- installation_scripts/init_installation.sh
+++ installation_scripts/init_installation.sh
@@ -30,13 +30,6 @@ sudo apt install -y pulseaudio pulsemixer
 # Neofetch/HTOP
 sudo apt install -y htop
 
-# Printing and bluetooth (if needed)
-sudo apt install -y cups
-sudo apt install -y bluez blueman
-
-sudo systemctl enable bluetooth
-sudo systemctl enable cups
-
 # Desktop background browser/handler 
 # feh --bg-fill /path/to/directory 
 # example if you want to use in autostart.sh for i3-gaps
@@ -46,14 +39,7 @@ sudo apt install -y feh
 sudo apt install -y rofi picom unzip polybar
 
 # Install i3 gaps
-sudo apt install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status
-
-git clone https://github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-mkdir -p build && cd build
-meson --prefix /usr/local
-ninja
-sudo ninja install
+sudo apt install -y i3-wm
 
 # Lightdm
 sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
